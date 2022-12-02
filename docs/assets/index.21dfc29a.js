1919bb9f3ef3832836f01e177a99e646e64a14c0
zouzhibing <esoul@654321>
2022-12-02 22:36:56 +0800

    build: 新包

--- docs/assets/index.21dfc29a.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,r as e,o as s,c as r,b as t}from"./index.50eb5827.js";const _={},a={style:{padding:"30px"}};function l(i,p){const o=e("el-alert"),c=e("router-view");return s(),r("div",a,[t(o,{title:"menu 1-2",type:"success",closable:!1}),t(c)])}const m=n(_,[["render",l]]);export{m as default};
