91e11aa0d3b0b575cf4fb03163a6b95a80c49c63
zouzhibing <zouzhibing@anthead.cn>
2023-03-02 10:38:35 +0800

    feat：发布新版本

---
+++ docs/assets/index.edf1d724.js
@@ -0,0 +1,1 @@
+import{_ as n,r as e,o as s,c as r,b as t}from"./index.12d7036b.js";const _={},a={style:{padding:"30px"}};function l(i,p){const o=e("el-alert"),c=e("router-view");return s(),r("div",a,[t(o,{title:"menu 1-2",type:"success",closable:!1}),t(c)])}const m=n(_,[["render",l]]);export{m as default};
